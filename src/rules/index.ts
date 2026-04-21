dd74cd2a9caeb3193f5e31ae0e65739ad5853f92
山东解大劦 <9727005@qq.com>
2026-04-21 18:05:44 +0800

    feat: 注册 FrontmatterRule 到规则注册中心

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,5 +1,6 @@
 // src/rules/index.ts
 import type { RuleRegistry } from '../core/RuleRegistry';
+import { FrontmatterRule } from './FrontmatterRule';
 import { HeadingRule } from './HeadingRule';
 import { ParagraphRule } from './ParagraphRule';
 import { ListRule } from './ListRule';
@@ -11,6 +12,7 @@ import { LinkRule } from './LinkRule';
  * 注册内置规则到规则注册中心
  */
 export function registerBuiltinRules(registry: RuleRegistry): void {
+  registry.register(new FrontmatterRule());
   registry.register(new HeadingRule());
   registry.register(new ParagraphRule());
   registry.register(new ListRule());
@@ -19,6 +21,7 @@ export function registerBuiltinRules(registry: RuleRegistry): void {
   registry.register(new LinkRule());
 }
 
+export { FrontmatterRule } from './FrontmatterRule';
 export { HeadingRule } from './HeadingRule';
 export { ParagraphRule } from './ParagraphRule';
 export { ListRule } from './ListRule';
